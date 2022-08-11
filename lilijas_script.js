94e551e63d39d251662f2d0a5a69d88a23b934b2
RomansAntonovics <romans.antonovics@gmail.com>
2022-08-11 18:24:23 +0300

    form

--- lilijas_script.js
+++ lilijas_script.js
@@ -1,6 +1,7 @@
 "use strict"
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
@@ -10,18 +11,52 @@ $(document).ready(function() {
 //первоначальный HTML документ будет полностью загружен и разобран, 
 //без ожидания полной загрузки таблиц стилей, изображений и фреймов.
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-e.prevenDefault();
+    async function formSend(event) {
+        event.preventDefault();
 
-let error = formValidate(form);
+        let error = formValidate(form);
     }
+
     function formValidate(form) {
         let error = 0;
         let formReq = document.querySelectorAll('._req');
+
+        for (let i = 0; i < formReq.length; i++) {
+            const input = formReq[i];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+	}
+    
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value); //regex поверяет правильность заполнения поля email
     }
 }
-) ;
+);
