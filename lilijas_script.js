e036f8cba0022f7709b6b18f50422406b068d563
RomansAntonovics <romans.antonovics@gmail.com>
2022-09-12 21:48:01 +0300

    form updating

--- lilijas_script.js
+++ lilijas_script.js
@@ -12,13 +12,34 @@ $(document).ready(function () {
 //без ожидания полной загрузки таблиц стилей, изображений и фреймов.
 
 document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
 
     async function formSend(event) {
         event.preventDefault();
 
         let error = formValidate(form);
+
+        let formData = new FormData(form); //вытягивает все данные из заполненных полей
+        formData.append('image', formImage.files[0]); //добавляем в переменную изображение
+
+        if (error === 0) { //при успешной валидации запускается ajax
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+
+            });
+            if (response.ok) {
+
+            } else {
+
+            } // *******************************
+
+
+        } else {
+            alert('Lūdzu, aizpildiet obligātus laukus');
+
+        }
     }
 
     function formValidate(form) {
@@ -45,8 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
         return error;
-	}
-    
+    }
+
     function formAddError(input) {
         input.parentElement.classList.add('_error');
         input.classList.add('_error');
@@ -58,5 +79,41 @@ document.addEventListener('DOMContentLoaded', function () {
     function emailTest(input) {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value); //regex поверяет правильность заполнения поля email
     }
+
+
+    //Получаем инпут file в переменную
+    const formImage = document.getElementById('formImage');
+    //Получаем див для превью в переменную
+    const formPreview = document.getElementById('formPreview');
+
+    //Слушаем изменения в инпуте file
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+    });
+
+    function uploadFile(file) {
+        // проверка типа файла
+        if (!['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/jpg'].includes(file.type)) {
+            alert("Drīkst pievienot tikai attēlus");
+            formImage.value = '';
+            return;
+        }
+        // проверка размера файла (<2 Мб)
+        if (file.size > 2 * 1024 * 1024) {
+            alert("Attēla izmēram jābūt mazāk par 2Mb");
+            return;
+        }
+
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="Photo">`;
+        };
+        reader.onerror = function (e) {
+            alert('Kļūda!');
+        };
+        reader.readAsDataURL(file);
+
+    }
+
 }
 );
